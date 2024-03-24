49bb0e3776613eb6b21e94ba5eafdd0c522b6dc1
smnrkzl <kizilsemanur@gmail.com>
2024-03-24 23:05:09 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+
+
+
+Console.WriteLine();
+
+Question1 question1 = new Question1();
+question1.answer1();
---
+++ ConsoleApp1/Question1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Question1
+    {
+        public  void answer1()
+        {
+
+            //Write an application that returns the sum of two numbers
+
+            /* 
+             1st form of writing:
+             int number1 = 10;
+             int number2 = 20;
+
+             int toplam = number1 + number2;
+
+             Console.WriteLine("The sum of two numbers is:" + toplam);*/
+
+            //2nd form of writing
+            int number1 = 15, number2 = 25;
+            Console.WriteLine(number1 + number2);
+        }
+    }
+}
