f654f2dc3fa9ab2473aabc86bb1600099767bf16
smnrkzl <kizilsemanur@gmail.com>
2024-03-27 23:59:54 +0300

    Question2Answer1

---
+++ ConsoleApp1/Question2inAnswer1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmQuestions
+{
+    public class Question2inAnswer1
+    {
+        public void answer1()
+        {
+            //Write an application that returns the sum of squares of two numbers entered by the user.
+            Console.WriteLine("Please enter number1");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter number2");
+            int number2 = int.Parse(Console.ReadLine());
+
+            int result = number1 * number1 + number2 * number2;
+            Console.WriteLine(result);
+        }
+    }
+}
