5e59e225017bedaec154f42bb976c8f9964fe2a2
smnrkzl <kizilsemanur@gmail.com>
2024-03-31 23:06:32 +0300

    3rd answer to the 2nd question

---
+++ ConsoleApp1/Question2Answer3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmQuestions
+{
+    public class Question2Answer3
+    {
+        public void question2Answer3() {
+
+
+            //Write an application that returns the sum of squares of two numbers entered by the user.
+            Console.WriteLine("Please enter number1 and number2");
+
+            Console.WriteLine(Math.Pow(int.Parse(Console.ReadLine()), 2) + Math.Pow(int.Parse(Console.ReadLine()), 2));
+
+
+        }
+    }
+}
